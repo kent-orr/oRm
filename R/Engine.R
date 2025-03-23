80ce43788e6504fff746d0a361ff2487779d4c9c
kent-orr <kent.orr@gmail.com>
2025-03-23 17:22:20 -0400

    introduction of pooling option

--- R/Engine.R
+++ R/Engine.R
@@ -6,87 +6,85 @@
 #' 
 #' @include TableModel.R
 #'
-#' @export
+#' @import pool
 Engine <- R6::R6Class(
   "Engine",
   public = list(
-    #' @field conn_args List of connection arguments
     conn_args = NULL,
-
-    #' @field conn Active database connection
     conn = NULL,
+    use_pool = FALSE,
 
-    #' @description
-    #' Initialize a new Engine instance
-    #' @param ... Connection arguments passed directly
-    #' @param conn_args List of connection arguments
-    initialize = function(..., conn_args = list()) {
+    #' Create an Engine object
+    #'
+    #' @param ... args to be passed to DBI::dbConnect
+    #' @param conn_args args to be passed to DBI::dbConnect
+    #' @param use_pool whether or not to make use of the pool package for ceonncetions to this engine
+    #' 
+    initialize = function(..., conn_args = list(), use_pool = FALSE) {
       dots <- list(...)
       if (length(dots) > 0) {
         self$conn_args <- dots
       } else {
         self$conn_args <- conn_args
       }
+      self$use_pool <- use_pool
     },
 
-    #' @description
-    #' Get or create a database connection
-    #' @param ... Additional connection arguments
-    #' @return A DBI connection object
-    get_connection = function(...) {
-      if (is.null(self$conn) || !DBI::dbIsValid(self$conn)) {
-        args <- modifyList(self$conn_args, list(...))
-        self$conn <-
-          do.call(DBI::dbConnect, args)
+    #' Get a connection to the database
+    #'
+    #' @return A DBIConnection object or a pool object
+    get_connection = function() {
+      if (is.null(self$conn)) {
+        if (self$use_pool) {
+          self$conn <- do.call(pool::dbPool, self$conn_args)
+        } else {
+          self$conn <- do.call(DBI::dbConnect, self$conn_args)
+        }
       }
       self$conn
     },
 
-    #' @description
-    #' Close the database connection
+    #' Close the database connection or pool
     close = function() {
-      if (!is.null(self$conn) && DBI::dbIsValid(self$conn)) {
-        DBI::dbDisconnect(self$conn)
+      if (!is.null(self$conn)) {
+        if (self$use_pool) {
+          pool::poolClose(self$conn)
+        } else if (DBI::dbIsValid(self$conn)) {
+          DBI::dbDisconnect(self$conn)
+        }
         self$conn <- NULL
       }
     },
 
-    #' @description
-    #' List all tables in the database
-    #' @return A character vector of table names
+    #' list tables in the database connection
     list_tables = function() {
-      con <- self$get_connection()
-      DBI::dbListTables(con)
+      on.exit(if (!self$use_pool) self$close())
+      DBI::dbListTables(self$get_connection())
     },
 
-    #' @description
-    #' Execute a SQL query and return results
-    #' @param sql SQL query string
-    #' @param ... Additional arguments passed to get_connection
-    #' @return A data frame containing query results
-    get_query = function(sql, ...) {
-      con <- self$get_connection(...)
-      DBI::dbGetQuery(con, sql)
+    #' Execute a SQL query and return the result as a data.frame
+    #'
+    #' @param sql SQL query
+    #' @return A data.frame
+    get_query = function(sql) {
+      on.exit(if (!self$use_pool) self$close())
+      DBI::dbGetQuery(self$get_connection(), sql)
     },
 
-    #' @description
-    #' Execute a SQL statement
-    #' @param sql SQL statement string
-    #' @param ... Additional arguments passed to get_connection
-    #' @return The number of rows affected
-    execute = function(sql, ...) {
-      con <- self$get_connection(...)
-      DBI::dbExecute(con, sql)
+    #' Execute a SQL query and return the number of rows affected
+    execute = function(sql) {
+      on.exit(if (!self$use_pool) self$close())
+      DBI::dbExecute(self$get_connection(), sql)
     },
 
-    #' @description
-    #' Create a new TableModel instance for a given table
+    #' Create a new TableModel object for the specified table
+    #'
     #' @param tablename Name of the table
-    #' @param ... Additional arguments passed to TableModel$new()
-    #' @return A new TableModel instance
-    model = function(tablename, ...) {
-      TableModel$new(tablename = tablename, engine = self, ...)
+    #' @param ... Additional arguments passed to the TableModel constructor
+    #' @param .data a named list of the arguments for the TableModel constructor
+    model = function(tablename, ..., .data=list()) {
+      TableModel$new(tablename = tablename, engine = self, ..., .data=.data)
     }
   )
-
 )
+
